f7702b7e55fb5ff609c0f1854132ed06abd24209
Ahmadkaissoon <ahmad.kaissoon@gmail.com>
2025-09-03 21:38:51 +0300

    generalization the filter bug in apis

--- src/api/reports/almostGone/getTotalAlmostGone.js
+++ src/api/reports/almostGone/getTotalAlmostGone.js
@@ -1,12 +1,13 @@
 import axiosClient from "../../../libs/axios/axios-client";
 
 async function getTotalAlmostGone({ queryKey }) {
-  const formData = new FormData();
-  Object.keys(queryKey[1]).map((key) => {
-    if (queryKey[1][key] !== null && queryKey[1][key] !== undefined)
-      formData.append(key, queryKey[1][key]);
-  });
-  const res = await axiosClient.post();
+  // const formData = new FormData();
+  // Object.keys(queryKey[1]).map((key) => {
+  //   if (queryKey[1][key] !== null && queryKey[1][key] !== undefined)
+  //     formData.append(key, queryKey[1][key]);
+  // });
+  const filter = queryKey[1];
+  const res = await axiosClient.get(``, { params: filter });
 
   return res?.data?.data;
 }
